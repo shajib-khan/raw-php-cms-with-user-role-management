e7f210d81f3a0d5084739abad3e3c6dea958c107
shajib-khan <shajibkhan946@gmail.com>
2024-06-03 21:15:49 +0600

    working on connection of database

---
+++ admin/add-user.php
@@ -0,0 +1,24 @@
+<?php
+    if(isset($_POST['submit'])){
+        $connection = mysqli_coun
+                        mysqli_param_count
+    }
+?>
+
+
+<form action="<?php $_SERVER['php_self']?>" method="POST">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+  
+    <input type="submit" value="Submit">
+  </form>
